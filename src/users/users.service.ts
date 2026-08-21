6eef3ec4510e7af847f43cf8420428ad1328c5f6
bandarwardi <bandarwardi@gmial.com>
2026-08-22 02:50:42 +0300

    fix: import NotFoundException

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException, BadRequestException } from '@nestjs/common';
+import { Injectable, ConflictException, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
